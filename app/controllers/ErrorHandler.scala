eba8c5acdfefa2cfa7ee7a65f72da45c7af99fdf
bosiakov <eugenebosyakov@gmail.com>
2020-03-27 22:31:22 +0300

    Error handler HTML render

--- app/controllers/ErrorHandler.scala
+++ app/controllers/ErrorHandler.scala
@@ -1,27 +1,22 @@
 package controllers
 
-import javax.inject.{Inject, Singleton}
-import play.api.http.HttpErrorHandler
+import javax.inject.Singleton
+import play.api.http.DefaultHttpErrorHandler
 import play.api.mvc.Results._
 import play.api.mvc._
-import play.api.i18n.Messages.Implicits._
-import play.api.i18n.MessagesApi
 
 import scala.concurrent._
 
 @Singleton
-class ErrorHandler @Inject() (val messagesApi: MessagesApi) extends HttpErrorHandler with play.api.i18n.I18nSupport {
-  private val logger = play.api.Logger(this.getClass)
+class ErrorHandler extends DefaultHttpErrorHandler {
+  override def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] =
+    Future.successful {
+      implicit val ir: RequestHeader = request
+      Status(statusCode)(views.html.error(message))
+    }
 
-  def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
-    Future.successful(
-      Status(statusCode)(views.html.error(request))
-    )
-  }
-
-  def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
-    Future.successful(
+  override def onServerError(request: RequestHeader, exception: Throwable): Future[Result] =
+    Future.successful {
       InternalServerError("A server error occurred: " + exception.getMessage)
-    )
-  }
+    }
 }
