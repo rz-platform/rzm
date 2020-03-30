4ad856bc536876d62c406565a07c32f1dc5941fb
bosiakov <eugenebosyakov@gmail.com>
2020-03-30 23:23:49 +0300

    Trailing slash filter implemented

---
+++ app/controllers/TrailingSlashFilter.scala
@@ -0,0 +1,29 @@
+package controllers
+
+import javax.inject.Singleton
+import play.api.Logger
+import play.api.http.Status.PERMANENT_REDIRECT
+import play.api.mvc.{EssentialAction, EssentialFilter, RequestHeader, Results}
+
+@Singleton
+class TrailingSlashFilter extends EssentialFilter {
+
+  private val logger = Logger(getClass)
+
+  override def apply(next: EssentialAction): EssentialAction = EssentialAction { req: RequestHeader =>
+    import play.api.libs.streams.Accumulator
+    if (req.path.endsWith("/") && req.path != "/") {
+      Accumulator.done(Results.Redirect(getPath(req), PERMANENT_REDIRECT))
+    } else {
+      next(req)
+    }
+  }
+
+  protected def getPath(req: RequestHeader): String = {
+    if (req.rawQueryString.isEmpty) {
+      req.path.stripSuffix("/")
+    } else {
+      s"${req.path.stripSuffix("/")}?${req.rawQueryString}"
+    }
+  }
+}
