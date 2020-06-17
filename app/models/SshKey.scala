1047e92ccd29908ac1ba9020b450df636dd67225
bosiakov <eugenebosyakov@gmail.com>
2020-06-17 22:41:01 +0300

    Ssh Keys page added

--- app/models/SshKey.scala
+++ app/models/SshKey.scala
@@ -4,9 +4,15 @@ import anorm.{ Macro, ToParameterList }
 
 case class SshKey(
   id: Int = 0,
-  publicKey: String,
-  title: Option[String]
+  publicKey: String
 )
 object SshKey {
   implicit def toParameters: ToParameterList[SshKey] = Macro.toParameters[SshKey]
 }
+
+case class SshKeyData(
+  publicKey: String
+)
+case class SshRemoveData(
+  id: Int
+)
