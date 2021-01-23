eb7cb9785ef96d2b69ae897dc6bf66cdad9d0e68
bosiakov <eugenebosyakov@gmail.com>
2021-01-23 14:42:55 +0300

    Tests restructuring completed

--- test/controllers/GenericControllerTest.scala
+++ test/controllers/GenericControllerTest.scala
@@ -2,16 +2,16 @@ package controllers
 
 import akka.actor.ActorSystem
 import models.{ Account, AccountRegistrationData, RzRepository, SessionName }
-import org.scalatest.{ BeforeAndAfterAll, PrivateMethodTester }
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{ Millis, Seconds, Span }
+import org.scalatest.{ BeforeAndAfterAll, PrivateMethodTester }
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
-import play.api.Configuration
 import play.api.mvc.Result
 import play.api.test.CSRFTokenHelper.addCSRFToken
-import play.api.test.Helpers.await
+import play.api.test.Helpers.{ await, defaultAwaitTimeout }
 import play.api.test.{ FakeRequest, Injecting }
+import play.api.{ Configuration, Logger }
 import repositories.{ AccountRepository, GitRepository, RzGitRepository }
 
 class GenericControllerTest
@@ -21,22 +21,25 @@ class GenericControllerTest
     with Injecting
     with PrivateMethodTester
     with ScalaFutures {
+  val logger: Logger = play.api.Logger(this.getClass)
 
   implicit val sys: ActorSystem = ActorSystem("RepositoryTest")
   implicit val defaultPatience: PatienceConfig =
     PatienceConfig(timeout = Span(5, Seconds), interval = Span(500, Millis))
-
-  private val logger = play.api.Logger(this.getClass)
-
-  implicit val sys: ActorSystem = ActorSystem("RepositoryTest")
 
   def getRandomString: String =
     java.util.UUID.randomUUID.toString.replace("-", "")
 
   def config: Configuration = app.injector.instanceOf[Configuration]
 
-  def accountController: AccountController          = app.injector.instanceOf[AccountController]
-  def gitEntitiesRepository: RzRepositoryController = app.injector.instanceOf[RzRepositoryController]
+  def accountController: AccountController             = app.injector.instanceOf[AccountController]
+  def collaboratorsController: CollaboratorsController = app.injector.instanceOf[CollaboratorsController]
+  def fileTreeController: FileTreeController           = app.injector.instanceOf[FileTreeController]
+
+  def accountRepository: AccountRepository = app.injector.instanceOf[AccountRepository]
+  def rzGitRepository: RzGitRepository     = app.injector.instanceOf[RzGitRepository]
+
+  def gitEntitiesController: RzRepositoryController = app.injector.instanceOf[RzRepositoryController]
   def git: GitRepository                            = app.injector.instanceOf[GitRepository]
 
   val sessionName: String = SessionName.toString
@@ -66,7 +69,7 @@ class GenericControllerTest
         .withFormUrlEncodedBody("name" -> name, "description" -> getRandomString)
         .withSession((sessionName, owner.id))
     )
-    val result = await(gitEntitiesRepository.saveRepository().apply(request))
+    val result = await(gitEntitiesController.saveRepository().apply(request))
     (result, new RzRepository(owner, name))
   }
 
--- test/controllers/RzRepositoryTest.scala
+++ test/controllers/RzRepositoryTest.scala
@@ -1,18 +1,19 @@
 package controllers
 
+import models.RzRepository
+import play.api.test.Helpers.{ await, defaultAwaitTimeout, LOCATION }
+
 class RzRepositoryTest extends GenericControllerTest {
-  "Repository Controller must" {
-    "Create Repository" in {
-      val account  = createAccount()
-      val repoName = getRandomString
+  "Create Repository" in {
+    val account  = createAccount()
+    val repoName = getRandomString
 
-      val (result, _) = createRepository(controller, repoName, account)
-      result.header.headers(LOCATION) must equal(
-        routes.GitEntitiesController.emptyTree(account.userName, repoName, RzRepository.defaultBranch).toString
-      )
+    val (result, _) = createRepository(repoName, account)
+    result.header.headers(LOCATION) must equal(
+      routes.FileTreeController.emptyTree(account.userName, repoName, RzRepository.defaultBranch).toString
+    )
 
-      val r = await(gitEntitiesRepository.getByOwnerAndName(account.userName, repoName))
-      r.isRight must equal(true)
-    }
+    val r = await(rzGitRepository.getByOwnerAndName(account.userName, repoName))
+    r.isRight must equal(true)
   }
 }
