9ab08bfd1969b16085681dd9d6d7ce7c36a59a36
Eugene <eugenebosyakov@gmail.com>
2021-06-13 22:23:38 +0300

    Timezone bug fixed

--- frontend/app/timezone.ts
+++ frontend/app/timezone.ts
@@ -1,7 +1,20 @@
+import { TurboEvent } from './types';
+
 // See https://caniuse.com/?search=Intl%20DateTimeFormat
 const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-const input = <HTMLInputElement>document.getElementById('timezone');
 
-if (input && timezone) {
-  input.value = timezone;
+function setTz() {
+  const input = <HTMLInputElement>document.getElementById('timezone');
+  if (input && timezone) {
+    input.value = timezone;
+  }
 }
+
+document.addEventListener('turbolinks:load', ((turboEvent: TurboEvent) => {
+  const url = turboEvent.data.url;
+  if (url && (url.indexOf('signin') > -1 || url.indexOf('signup') > -1)) {
+    setTz();
+  }
+}) as EventListener);
+
+setTz();
